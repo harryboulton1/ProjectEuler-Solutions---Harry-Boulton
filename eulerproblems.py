8258c1f431118e63af06f66c24b335cffde274b1
harryboulton1 <harryboulton10@gmail.com>
2017-06-04 15:23:11 +0100

    Update

--- eulerproblems.py
+++ eulerproblems.py
@@ -871,8 +871,65 @@ def p39():
 
     return max(solutions, key=solutions.get)
 
+def p43():
+    import itertools
+    permutations = [list(i) for i in itertools.permutations([0,1,2,3,4,5,6,7,8,9])]
+
+    primes = [2,3,5,7,11,13,17]
+    def concat(digits, i):
+        return int(str(digits[i+1])+str(digits[i+2])+str(digits[i+3]))
+
+    solutions = []
+    for n in permutations:
+        digits = {}
+        for digit in n:
+            digits[digit+1] = n[digit]
+
+
+        count = 0
+        for i in range(1,8):
+            #print(i+1, i+2, i+3)
+            #print(primes[i-1])
+            triplet = concat(digits,i)
+            if triplet % primes[i-1] == 0:
+                count += 1
+            #if int("".join(digits[i+1],digits[i+2],digits[i+3])) % primes[i-1] == 0:
+            #    count += 1
+                #print(count)
+
+        if count == 7:
+            solution = int(''.join(str(i) for i in n))
+            solutions.append(solution)
+
+    return sum(solutions)
+
+
 def p48():
     total = 0
     for i in range(1,1000+1):
         total += i**i
     return total
+
+def p53():
+    def fct(n):
+        x = 1
+        for i in range(n):
+            x *= i+1
+        return x
+
+    def c(n,r):
+        if type(n) != int or type(r) != int:
+            print("n and r must be integers")
+        else:
+            return int(fct(n) / (fct(r)*fct(n-r)))
+
+    count = 0
+    for n in range(1,101):
+        for r in range(n):
+            if c(n,r) > 1000000:
+                count += 1
+
+    return count
+
+
+print(p53())
