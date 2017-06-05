f329aaf082b5f56887f5cea173ea4eb19f2518f0
harryboulton1 <harryboulton10@gmail.com>
2017-06-05 19:00:53 +0100

    Update
    Completed 55

--- SOLUTIONS (README).txt
+++ SOLUTIONS (README).txt
@@ -1,6 +1,6 @@
 
 # Working solutions, solve in <1 minute
-Complete solutions: 1, 2, 4, 6, 7, 8, 10, 11, 12, 14, 16, 20, 22, 29, 30, 34, 35, 36, 43, 48, 52, 53
+Complete solutions: 1, 2, 4, 6, 7, 8, 10, 11, 12, 14, 16, 20, 22, 29, 30, 34, 35, 36, 43, 48, 52, 53, 55
 
 # Working solutions which take a LONG time to compute
 Semi-complete solutions: 3, 5, 9, 21, 23, 39
--- eulerproblems.py
+++ eulerproblems.py
@@ -944,4 +944,43 @@ def p53():
     return count
 
 
-print(p52())
+def p55():
+    def isPalindrome(n):
+        n_split = [int(i) for i in str(n)]
+        for i in range(len(n_split)):
+            if n_split[i] == n_split[-i-1]:
+                pass
+            else:
+                return False
+        return True   
+
+    def reverse(n):
+        n_list = [int(x) for x in str(n)]
+        r_list = []
+        for i in range(len(n_list)):
+            r_list.append(n_list[len(n_list)-i-1])
+        r = "".join(str(d) for d in r_list)
+
+
+        return int(r)
+
+    def isLychrel(n):
+        if isPalindrome(n+reverse(n)) == True:
+            return False
+        return True
+
+    lychrels = 0
+    for n in range(10000):
+
+        for i in range(50):
+
+            if isLychrel(n) == False:
+                break
+            else:
+                n = n+reverse(n)
+            if i == 49:
+                lychrels += 1
+                
+
+    return lychrels
+
